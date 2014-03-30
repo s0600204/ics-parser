8a19cc38dcb61ea11f8b232bee24624f5f7baa62
s0600204 <dev@s0600204>
2014-03-30 22:33:43 +0100

    (p) Amend sort function in class.iCalParser.php
    * Now matches and overrides the parent function properly
    * Also extends the function so it, like its parent, can be told to sort by DTEND instead of the default DTSTART

--- class.iCalParser.php
+++ class.iCalParser.php
@@ -210,6 +210,8 @@ class ParsedICal extends ICal
                 && is_array($icalDate['params'])
                 && isset($icalDate['params']['TZID']))
             {
+                $eventTZ = $icalDate['params']['TZID'];
+            }
             $icalDate = $icalDate['value'];
         }
         
@@ -300,39 +302,44 @@ class ParsedICal extends ICal
     }
     
     /**
-     * Sorts and returns an array of events
+     * Sorts an array of events
+     * 
+     * This is a more accurate sort than that of its parent as it compares
+     * Unix-times. However, it does have a problem with events that fall
+     * after a particular date in 2038, that its parent does not.
      *
-     * @param {array} $events    An array with events.
-     * @param {array} $sortOrder Either SORT_ASC, SORT_DESC, SORT_REGULAR, 
-     *                           SORT_NUMERIC, SORT_STRING
-     *
-     * @return {array}
+     * @param {array} &$events   An array with events.
+     * @param {array} $sortKey   Which date-time to sort by (DTSTART, DTEND, DTSTAMP)
+     * @param {array} $sortOrder Either SORT_ASC or SORT_DESC
      */
-    public function sortEvents ($events, $sortOrder = SORT_ASC)
+    public function sortEvents (&$events, $sortKey = "DTSTART", $sortOrder = SORT_ASC)
     {
-        $extendedEvents = array();
-        
-        // loop through all events, adding two new elements
-        foreach ($events as $anEvent) {
-            if (!array_key_exists('UNIX_TIMESTAMP', $anEvent)) {
-                $anEvent['UNIX_TIMESTAMP'] = 
-                            $this->iCalDateToUnixTimestamp($anEvent['DTSTART']);
-            }
-
-            if (!array_key_exists('REAL_DATETIME', $anEvent)) {
-                $anEvent['REAL_DATETIME'] = 
-                            date("d.m.Y", $anEvent['UNIX_TIMESTAMP']);
-            }
-            
-            $extendedEvents[] = $anEvent;
+        if ($sortOrder !== SORT_ASC && $sortOrder !== SORT_DESC) {
+            // todo: set error
+            return;
         }
         
-        foreach ($extendedEvents as $key => $value) {
-            $timestamp[$key] = $value['UNIX_TIMESTAMP'];
+        $evDTstamp = array();
+        foreach ($events as $event) {
+            switch ($sortKey) {
+            case "DTSTAMP":
+                $dt = $event["dtstamp"];
+                break;
+            case "DTEND":
+                if (isset($event["dtend"])) {
+                    $dt = $event["dtend"];
+                    break;
+                }
+            case "DTSTART":
+            default:
+                $dt = $event["dtstart"];
+                break;
+            }
+            $evDTstamp[$event["uid"].$dt] = $dt;
         }
-        array_multisort($timestamp, $sortOrder, $extendedEvents);
-
-        return $extendedEvents;
+        
+        array_multisort($evDTstamp, $sortOrder, $events);
     }
+    
 }
 ?>
--- dev-tests/SortTest.php
+++ dev-tests/SortTest.php
@@ -3,6 +3,11 @@
 date_default_timezone_set("UTC");
 
 require 'class.iCalReader.php';
+$ical = new ICal('MyCal.ics');
+/*
+require 'class.iCalParser.php';
+$ical = new ParsedICal('MyCal.ics');
+*/
 
 ?>
 <DOCTYPE html>
@@ -13,85 +18,68 @@ require 'class.iCalReader.php';
 <body>
 <?php
 
-$ical   = new ICal('../MyCal.ics');
 $events = $ical->getEvents();
+
+function echoTimes ($events) {
+	echo "<pre>\n";
+	echo "<b>UID\t\t\t\tDTSTAMP\t\t\tDTSTART\t\t\tDTEND</b>\n";
+	foreach ($events as $event) {
+		if (isset($event['dtstart'])) {
+			$uid = $event['uid'];
+		//	$dtstamp = date("Ymd His", $event['dtstamp']);
+			$dtstamp = "00000000T000000";
+			$dtstart = date("Ymd His", $event['dtstart']);
+			$dtend = date("Ymd His", $event['dtend']);
+		} else {
+			$uid = $event['UID']['value'];
+			$dtstamp = $event['DTSTAMP']['value'];
+			$dtstart = $event['DTSTART']['value'];
+			$dtend = $event['DTEND']['value'];
+		}
+		
+		echo "<i>" . substr($uid, 0, strpos($uid,'@')) . "</i>\t";
+		
+		echo substr($dtstamp,0,4)  . "-" .
+			 substr($dtstamp,4,2)  . "-" .
+			 substr($dtstamp,6,2)  . " " .
+			 substr($dtstamp,9,2)  . ":" .
+			 substr($dtstamp,11,2) . ":" .
+			 substr($dtstamp,13,2) . "\t";
+		
+		echo substr($dtstart,0,4)  . "-" .
+			 substr($dtstart,4,2)  . "-" .
+			 substr($dtstart,6,2)  . " " ;
+		if (strlen($dtstart) > 10) {
+			echo substr($dtstart,9,2)  . ":".
+				 substr($dtstart,11,2) . ":".
+				 substr($dtstart,13,2) . "\t";
+		} else {
+			echo "\t\t";
+		}
+		
+		echo substr($dtend,0,4)  . "-" .
+			 substr($dtend,4,2)  . "-" .
+			 substr($dtend,6,2)  . " " ;
+		if (strlen($dtend) > 10) {
+			echo substr($dtend,9,2)  . ":".
+				 substr($dtend,11,2) . ":".
+				 substr($dtend,13,2);
+		}
+		echo "\n";
+	}
+	echo "</pre>\n";
+}
 
 echo "<p><b>Current Test:</b> sort (dtstart)</p>\n\n";
 echo "<hr>\n\n";
 
 echo "<p>Before:</p>\n";
-echo "<pre>\n";
-echo "<b>UID\t\t\t\tDTSTAMP\t\t\tDTSTART\t\t\tDTEND</b>\n";
-foreach ($events as $event) {
-	echo "<i>" . substr($event['UID']['value'], 0, strpos($event['UID']['value'],'@')) . "</i>\t";
-	
-	echo substr($event['DTSTAMP']['value'],0,4)  . "-" .
-		 substr($event['DTSTAMP']['value'],4,2)  . "-" .
-		 substr($event['DTSTAMP']['value'],6,2)  . " " .
-		 substr($event['DTSTAMP']['value'],9,2)  . ":" .
-		 substr($event['DTSTAMP']['value'],11,2) . ":" .
-		 substr($event['DTSTAMP']['value'],13,2) . "\t";
-	
-	echo substr($event['DTSTART']['value'],0,4)  . "-" .
-		 substr($event['DTSTART']['value'],4,2)  . "-" .
-		 substr($event['DTSTART']['value'],6,2)  . " " ;
-	if (strlen($event['DTSTART']['value']) > 10) {
-		echo substr($event['DTSTART']['value'],9,2)  . ":".
-			 substr($event['DTSTART']['value'],11,2) . ":".
-			 substr($event['DTSTART']['value'],13,2) . "\t";
-	} else {
-		echo "\t\t";
-	}
-	
-	echo substr($event['DTEND']['value'],0,4)  . "-" .
-		 substr($event['DTEND']['value'],4,2)  . "-" .
-		 substr($event['DTEND']['value'],6,2)  . " " ;
-	if (strlen($event['DTEND']['value']) > 10) {
-		echo substr($event['DTEND']['value'],9,2)  . ":".
-			 substr($event['DTEND']['value'],11,2) . ":".
-			 substr($event['DTEND']['value'],13,2);
-	}
-	echo "\n";
-}
-echo "</pre>\n";
+echoTimes($events);
 
 $ical->sortEvents($events);
 
 echo "<p>After:</p>\n";
-echo "<pre>\n";
-echo "<b>UID\t\t\t\tDTSTAMP\t\t\tDTSTART\t\t\tDTEND</b>\n";
-foreach ($events as $event) {
-	echo "<i>" . substr($event['UID']['value'], 0, strpos($event['UID']['value'],'@')) . "</i>\t";
-	
-	echo substr($event['DTSTAMP']['value'],0,4)  . "-" .
-		 substr($event['DTSTAMP']['value'],4,2)  . "-" .
-		 substr($event['DTSTAMP']['value'],6,2)  . " " .
-		 substr($event['DTSTAMP']['value'],9,2)  . ":" .
-		 substr($event['DTSTAMP']['value'],11,2) . ":" .
-		 substr($event['DTSTAMP']['value'],13,2) . "\t";
-	
-	echo substr($event['DTSTART']['value'],0,4)  . "-" .
-		 substr($event['DTSTART']['value'],4,2)  . "-" .
-		 substr($event['DTSTART']['value'],6,2)  . " " ;
-	if (strlen($event['DTSTART']['value']) > 10) {
-		echo substr($event['DTSTART']['value'],9,2)  . ":".
-			 substr($event['DTSTART']['value'],11,2) . ":".
-			 substr($event['DTSTART']['value'],13,2) . "\t";
-	} else {
-		echo "\t\t";
-	}
-	
-	echo substr($event['DTEND']['value'],0,4)  . "-" .
-		 substr($event['DTEND']['value'],4,2)  . "-" .
-		 substr($event['DTEND']['value'],6,2)  . " " ;
-	if (strlen($event['DTEND']['value']) > 10) {
-		echo substr($event['DTEND']['value'],9,2)  . ":".
-			 substr($event['DTEND']['value'],11,2) . ":".
-			 substr($event['DTEND']['value'],13,2);
-	}
-	echo "\n";
-}
-echo "</pre>\n";
+echoTimes($events);
 
 ?>
 </pre>
